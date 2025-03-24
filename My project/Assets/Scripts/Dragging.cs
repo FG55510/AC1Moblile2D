7fd33613f0a54493f74e0afa54efb237ebdfa70c
F.G.5510 <ferrangenos@gmail.com>
2025-03-24 02:12:01 -0300

    scripts modelos incluidos

---
+++ My project/Assets/Scripts/Dragging.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragging : MonoBehaviour
+{
+    private bool isDragging = false;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case UnityEngine.TouchPhase.Began:
+                    // Raycast2D para verificar se tocou no objeto
+                    RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
+
+                    if (hit.collider != null && hit.transform == transform) // Se tocou no objeto
+                    {
+                        isDragging = true;
+                    }
+                    break;
+
+                case UnityEngine.TouchPhase.Moved:
+                    if (isDragging)
+                    {
+                        transform.position = touchPosition;
+                    }
+                    break;
+
+                case UnityEngine.TouchPhase.Ended:
+                    isDragging = false;
+                    break;
+            }
+        }
+    }
+}
+
